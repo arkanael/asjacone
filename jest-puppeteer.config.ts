5ff5bfba3aeeb7bcee2864fae3fc5f607ac4439f
Juliana Ianov <juliaianov.mcsd@gmail.com>
2024-11-28 21:56:07 -0300

    ajustes na branch

--- jest-puppeteer.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'jest';
-
-const jestPuppeteerConfig = async (): Promise<Config> => {
-    return {
-        verbose: true,
-    };
-};
-
-export default jestPuppeteerConfig;
