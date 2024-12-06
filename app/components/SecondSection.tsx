bb78583ae3c8e4029ff35a48fcc04e6e064ea1c6
Luiz Guilherme Bandeira <arkanael@gmail.com>
2024-12-05 22:47:03 -0300

    Revert "ajustes"

--- app/components/SecondSection.tsx
+++ app/components/SecondSection.tsx
@@ -1,40 +1,14 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 export default function SecondSection() {
-  const [videoSource, setVideoSource] = useState("/Secondsection2.mp4");
-
-  useEffect(() => {
-    const updateVideoSource = () => {
-      if (window.innerWidth >= 1024) {
-        // Telas grandes (1024px ou mais)
-        setVideoSource("/Secondsection.mp4");
-      } else {
-        // Telas pequenas
-        setVideoSource("/Secondsection2.mp4");
-      }
-    };
-
-    // Configura o vídeo inicial
-    updateVideoSource();
-
-    // Adiciona um listener para mudanças no tamanho da tela
-    window.addEventListener("resize", updateVideoSource);
-
-    // Remove o listener quando o componente desmonta
-    return () => {
-      window.removeEventListener("resize", updateVideoSource);
-    };
-  }, []);
-
   return (
     <section className="w-full h-screen m-0 p-0 overflow-hidden relative top-0">
       <video
-        src={videoSource}
+        src="/Secondsection.mp4"
         autoPlay
         loop
         muted
-        className="m-0 p-0 w-full h-full object-cover"
+        className="m-0 p-0 w-full h-full  object-fill sm:h-screen md:h-screen lg:h-screen"  // Mudança de object-contain para object-cover
       />
     </section>
   );
